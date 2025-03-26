a12a968f3dacfd1ced934f429654cfbfb19c2765
Taras Zelenyi <tzelenyy@gmail.com>
2025-03-26 22:18:22 +0200

    fix 7 Furniture Web App

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Hero from "./components/Hero/Hero";
 import Features from "./components/Features/Features";
 import Shop from "./components/Shop/Shop";
 import Advantages from "./components/Advantages/Advantages";
-import ImageGenerator from "./components/ImageGenerator/ImageGenerator";
 import Reviews from "./components/Reviews/Reviews";
 import Footer from "./components/common/Footer/Footer";
 
@@ -18,7 +17,6 @@ const App: FC<AppProps> = () => (
     <Features />
     <Shop />
     <Advantages />
-    <ImageGenerator />
     <Reviews />
     <Footer />
   </div>
